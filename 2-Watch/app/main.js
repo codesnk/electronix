333de89a2d50bd0a4411cdad47a578b278744c96
Shah Nawaz Khan <shazmailz@gmail.com>
2019-10-14 10:16:41 +0200

    minor updates

--- 2-Watch/app/main.js
+++ 2-Watch/app/main.js
@@ -1,17 +1,51 @@
 const {
     app,
-    BrowserWindow
-} = require('electron')
+    BrowserWindow,
+    dialog
+} = require('electron') // dialog is for opening system dialog boxes
+const fs = require('fs') // for reading files
+
+let mainWindow = null
 
 function mainInterface() {
-    let mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 500,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        show: false
     })
     mainWindow.loadFile('app/index.html')
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+        //getFiles()
+    })
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+// This func is only here coz I was testing dialogboxes. Not needed for the watch app
+const getFiles = () => {
+    const files = dialog.showOpenDialog(mainWindow, {
+        properties: ['openFile'],
+        filters: [{
+                name: 'Text Files',
+                extensions: ['txt']
+            },
+            {
+                name: 'Markdown Files',
+                extensions: ['md', 'markdown']
+            }
+        ]
+    })
+    if (!files) {
+        return
+    }
+    console.log(files)
+
 }
 
 app.on('ready', mainInterface)
